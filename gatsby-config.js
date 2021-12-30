1001c719679a225484d91bb335031fc1096b6fe3
Mar-Issah <masy370@gmail.com>
2021-12-30 10:28:26 -0400

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  siteMetadata: {
+    author: "Marsiya",
+    title: "Siya Gatsby Website",
+  },
+  /* Your site config here */
+  // plugins: [`gatsby-plugin-typography`],
+  // plugins: [
+  //   {
+  //     resolve: `gatsby-plugin-typography`,
+  //   },
+  // ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const Home = () => {
+  return (
+    <div>
+      <h1> Hello world!</h1>
+    </div>
+  )
+}
+
+export default Home
