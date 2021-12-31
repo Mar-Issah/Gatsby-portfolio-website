21ea0844ecdb65cedf04d62803660d91e454a99e
Mar-Issah <masy370@gmail.com>
2021-12-31 10:48:38 -0400

    added navbar image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,16 @@ const Navbar = () => {
   return (
     <nav class="navbar navbar-expand-lg navbar-light sticky-top">
       <div class="container">
-        <Link class="navbar-brand" to="/">
-          <img src="www.freepnglogos.com/pics/coffee-logo-png" alt="logo" />
+        <Link
+          href="https://www.freepnglogos.com/pics/coffee-logo-png"
+          title="Image from freepnglogos.com"
+        >
+          <img
+            className="img"
+            src="https://www.freepnglogos.com/uploads/coffee-logo-png/coffee-logo-design-creative-idea-logo-elements-2.png"
+            width="100"
+            alt="coffee logo design creative idea logo elements"
+          />
         </Link>
         <div class="links">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
